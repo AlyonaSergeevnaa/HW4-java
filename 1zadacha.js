d90add87c1fb625d3aaddb0aebd5cd2d84440640
Alyona <molokowa.alyona2010@yandex.ru>
2023-06-15 18:41:10 +0300

    add files

---
+++ 1zadacha.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+<link rel="stylesheet"
+<meta charset="UTF-8">
+<title>HW-4</title>
+<script>
+
+</script>
+</head>
+
+<body>
+<h1>Домашнее задание -  Создать страницу, которая спрашивает имя у пользователя и выводит его с помощью функции.</h1>
+<script>
+var name = prompt("Ваше имя?", "");
+alert("Добрый день - " + name + "!");
+</script>
+
+</body>
+
+</html>
---
+++ 2zadacha.js
@@ -0,0 +1,19 @@
+let age = 101;
+switch (age) 
+{
+case 18:
+console.log('Вы совершеннолетний, все можно!');
+break;
+
+case 10:
+console.log('Вам надо учить уроки!');
+break;
+
+case 30:
+console.log('Ложитесь спать, завтра на работу');
+break;
+
+default:
+console.log('Мы не знаем что Вам делать');
+break;
+}
